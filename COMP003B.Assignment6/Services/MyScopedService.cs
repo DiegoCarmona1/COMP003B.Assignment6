c9413092ba4b02adb68b39fcc81518b4af66be1b
Diego Carmona <ultgojifan@gmail.com>
2024-05-02 14:07:54 -0700

    Add project files.

---
+++ COMP003B.Assignment6/Services/MyScopedService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment6.Services
+{
+    public class MyScopedService : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
